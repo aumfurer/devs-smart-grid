caad59cdd4026c520ee7df90df775e1519cfe0a3
aumfurer <alfredoumfurer@hotmail.com>
2018-09-24 00:21:56 -0300

    bateria.* compiles, but was not tested

---
+++ simulation/src/bateria.cpp
@@ -0,0 +1,121 @@
+#include <random>
+#include <string>
+#include <cassert>
+#include <cmath>
+
+#include "message.h"
+#include "parsimu.h"
+#include "real.h"
+#include "tuple_value.h"
+
+#include "bateria.h"
+
+using namespace std;
+
+
+Bateria::Bateria(const string &name) :
+	Atomic(name),
+
+	energy_in(addInputPort("energy_in")),
+	required_energy(addInputPort("required_energy")),
+	batery_state(addOutputPort("batery_state")),
+
+	energy_from_generators(0),
+    energy_sending(0),
+    charge(0),
+    last_update(0),
+	next_state(Bateria::AVAILABLE)
+{
+}
+
+Model &Bateria::initFunction()
+{
+	holdIn(AtomicState::active, VTime::Inf);
+	return *this;
+}
+
+
+Model &Bateria::externalFunction(const ExternalMessage &msg)
+{
+	this->update_current_charge(msg.time());
+	double value = Real::from_value(msg.value()).value();
+	if(msg.port() == this->energy_in){
+		this->energy_from_generators += value;
+	} else if(msg.port() == this->required_energy){
+		this->energy_sending = this->next_state == Bateria::AVAILABLE ? 0: value;
+	}
+	this->update_next_event();
+	return *this;
+}
+
+
+Model &Bateria::internalFunction(const InternalMessage &msg)
+{
+	VTime update_time = msg.time();
+
+	if (this->next_state == Bateria::EMPTY){
+		// Se termino la bateria
+		this->energy_sending = 0;
+		this->charge = 0;
+		if(this->energy_from_generators > 0){
+			nextChange(to_VTime(Bateria::MIN_CAPACITY / this->energy_from_generators));			 
+		} else {
+			nextChange(VTime::Inf);
+		}
+	} else if (this->next_state == Bateria::MIN_CAPACITY){
+		// Ya esta disponible la bateria: recalculo el tiempo para que se llene
+		this->next_state = Bateria::FULL;
+		const float delta = this->energy_from_generators - this->energy_sending;
+		if (delta > 0){
+			const float time_to_full =  (Bateria::CAPACITY - this->charge) / delta;
+			nextChange(to_VTime(time_to_full));
+		} else {
+			nextChange(VTime::Inf);
+		}
+	} else {
+		// Se lleno la bateria (no hago nada?)
+		assert(this->energy_from_generators > this->energy_sending);
+		nextChange(VTime::Inf);
+	}
+	return *this;
+}
+
+
+Model &Bateria::outputFunction(const CollectMessage &msg)
+{
+	// estoy aca porque ocurrio un evento
+	sendOutput(msg.time(), this->batery_state, Real(this->next_state));
+	
+	return *this ;
+}
+
+void Bateria::update_current_charge(const VTime &update_time)
+{
+	const float delta = this->energy_from_generators - this->energy_sending;
+	this->charge += delta * (update_time - this->last_update).asMsecs();
+	this->last_update = update_time;
+}
+
+void Bateria::update_next_event()
+{
+	const float delta = this->energy_from_generators - this->energy_sending;
+	if(delta == 0){
+		nextChange(VTime::Inf); 
+	} else if (delta < 0){
+		const float remaining_hours = this->charge / -delta;
+		this->next_state = Bateria::EMPTY;
+		nextChange(to_VTime(remaining_hours));
+	} else if (this->charge < Bateria::MIN_CAPACITY){
+		const float time_to_availability = (Bateria::MIN_CAPACITY - this->charge) / delta;
+		this->next_state = Bateria::AVAILABLE;
+		nextChange(to_VTime(time_to_availability));
+	} else {
+		const float time_to_full =  (Bateria::CAPACITY - this->charge) / delta;
+		this->next_state = Bateria::FULL;
+		nextChange(to_VTime(time_to_full));
+	}
+}
+
+VTime Bateria::to_VTime(double v){
+	return VTime((int)ceil(60 * 60 * 1000 * v));
+}
---
+++ simulation/src/bateria.h
@@ -0,0 +1,49 @@
+#ifndef _BATERIA_H_
+#define _BATERIA_H_
+
+#include <random>
+
+#include "atomic.h"
+#include "VTime.h"
+
+
+#define BATERIA_NAME "bateria"
+
+
+class Bateria : public Atomic {
+  public:
+
+    Bateria(const string &name = BATERIA_NAME );
+    virtual string className() const {  return BATERIA_NAME ;}
+  
+    const double CAPACITY = 500.0;
+    const double MIN_CAPACITY = 10.0;
+
+    const int EMPTY = 0;
+    const int AVAILABLE = 1;
+    const int FULL = 2;
+
+  protected:
+    Model &initFunction();
+    Model &externalFunction( const ExternalMessage & );
+    Model &internalFunction( const InternalMessage & );
+    Model &outputFunction( const CollectMessage & );
+
+  private:
+    const Port &energy_in;
+    const Port &required_energy;
+    Port &batery_state;
+
+    double energy_from_generators;
+    double energy_sending;
+    double charge;
+    VTime last_update;
+    int next_state;
+
+    void update_current_charge(const VTime &update_time);
+    void update_next_event();
+
+    VTime to_VTime(double v);
+};
+
+#endif
