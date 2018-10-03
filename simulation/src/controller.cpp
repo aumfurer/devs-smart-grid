d8b218ca69e2185ecd97f7a321c682de4f6b1c4a
Pablo Balbi <pablo.l.balbi@gmail.com>
2018-10-03 18:58:28 -0300

    Controller.cpp template and compiling

--- simulation/src/controller.cpp
+++ simulation/src/controller.cpp
@@ -3,6 +3,7 @@
 Controller::Controller(const string &name) :
     Atomic(name),
 
+    // Input ports
     batteryState(addInputPort("battery_state")),
     loadDemand(addInputPort("load_demand")),
 
@@ -10,3 +11,19 @@ Controller::Controller(const string &name) :
     gridDemand(addOutputPort("grid_demand")),
     batteryDemand(addOutputPort("battery_demand"))
 {}
+
+Model& Controller::initFunction() {
+
+}
+
+Model& Controller::externalFunction( const ExternalMessage &aMessage) {
+
+}
+
+Model& Controller::internalFunction( const InternalMessage &aMessage) {
+
+}
+
+Model& Controller::outputFunction( const CollectMessage &aMessage) {
+
+}
