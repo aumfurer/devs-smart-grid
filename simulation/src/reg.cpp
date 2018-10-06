87b3210817d28b4f76e38ca69b6015b1350eb33e
Pablo Balbi <pablo.l.balbi@gmail.com>
2018-10-06 15:13:56 -0300

    Registering controller

--- simulation/src/reg.cpp
+++ simulation/src/reg.cpp
@@ -7,6 +7,7 @@
 #include "celda_solar.h"
 #include "generador_eolico.h"
 #include "bateria.h"
+#include "controller.h"
 #include "carga.h"
 
 
@@ -19,6 +20,7 @@ void register_atomics_on(ParallelModelAdmin &admin)
 	admin.registerAtomic(NewAtomicFunction<CeldaSolar>(), CELDA_SOLAR_NAME);
 	admin.registerAtomic(NewAtomicFunction<GeneradorEolico>(), GENERADOR_EOLICO_NAME);
 	admin.registerAtomic(NewAtomicFunction<Bateria>(), BATERIA_NAME);
+	admin.registerAtomic(NewAtomicFunction<Controller>(), CONTROLLER_NAME);
 	admin.registerAtomic(NewAtomicFunction<Carga>(), CARGA_NAME);
 
 }
