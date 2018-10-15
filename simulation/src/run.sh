1912ecbda9c62fc7a07cbd825b73723173911c6a
Pablo Balbi <pablo.l.balbi@gmail.com>
2018-10-15 13:47:55 -0300

    Script to run charge checking

--- simulation/src/run.sh
+++ simulation/src/run.sh
@@ -1,1 +1,9 @@
-bin/cd++ -m../batery.ma -e../sg.ev -ooutput -llogs
+#!/bin/bash
+# TODO: Put flags in order to choose .ma and .ev
+echo "Removing prevously used log files"
+rm -f /tmp/cliOutput
+rm -f /tmp/batteryChargeChanges
+echo "Running simulation"
+./bin/cd++ -e../eventGeneration/mergedData.ev -mmodels/controllerAndSolar.ma -oout/realData -lout/realData.log -t48:00:00:00 &> /tmp/cliOutput
+echo "Filtering battery charge changes"
+cat /tmp/cliOutput|grep NewCharge|less > /tmp/batteryChargeChanges
