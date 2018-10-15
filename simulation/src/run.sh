0b2a5fbbac195a683de6f5459c86fde463b6ce37
Pablo Balbi <pablo.l.balbi@gmail.com>
2018-10-15 18:03:17 -0300

    Simulating whole week

--- simulation/src/run.sh
+++ simulation/src/run.sh
@@ -4,6 +4,6 @@ echo "Removing prevously used log files"
 rm -f /tmp/cliOutput
 rm -f /tmp/batteryChargeChanges
 echo "Running simulation"
-./bin/cd++ -e../eventGeneration/mergedData.ev -mmodels/controllerAndSolar.ma -oout/realData -lout/realData.log -t48:00:00:00 &> /tmp/cliOutput
+./bin/cd++ -e../eventGeneration/mergedData.ev -mmodels/solarAndWindControllerModel.ma -oout/realData -lout/realData.log -t168:00:00:00 &> /tmp/cliOutput
 echo "Filtering battery charge changes"
 cat /tmp/cliOutput|grep NewCharge|less > /tmp/batteryChargeChanges
