67c12b05c4db50c21fff1fd343b6440cdca64879
Pablo Balbi <pablo.l.balbi@gmail.com>
2018-10-19 00:47:16 -0300

    Run script configured for saving experiments

--- simulation/src/run.sh
+++ simulation/src/run.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 # TODO: Put flags in order to choose .ma and .ev
+
+SIMULATOR=./bin/cd++
+OUTPUT_FOLDER=out
+
+if [[ $# -eq 0 ]]; then
+    echo "$0 modelFile eventsFile simulationName endingTime=7days"
+    exit 0
+fi
+
+ENDING_TIME=$4
+if [[ $4 -eq "" ]]; then
+    ENDING_TIME=168:00:00:00
+fi
+
+SIMULATION_NAME=$3
+MODEL_FILE=$1
+EVENTS_FILE=$2
+
 echo "Removing prevously used log files"
-rm -f /tmp/cliOutput
-rm -f /tmp/batteryChargeChanges
+# TODO
 echo "Running simulation"
-./bin/cd++ -e../eventGeneration/mergedData.ev -mmodels/solarAndWindControllerModel.ma -oout/realData -lout/realData.log -t168:00:00:00 &> /tmp/cliOutput
+
+$SIMULATOR -e$EVENTS_FILE -m$MODEL_FILE -o$OUTPUT_FOLDER/$SIMULATION_NAME -l$OUTPUT_FOLDER/$SIMULATION_NAME.log -t$ENDING_TIME &> /tmp/cliOutput
 echo "Filtering battery charge changes"
-cat /tmp/cliOutput|grep NewCharge|less > /tmp/batteryChargeChanges
+cat /tmp/cliOutput|grep NewCharge|less > $OUTPUT_FOLDER/batteryChargeChanges_$SIMULATION_NAME
